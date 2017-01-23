dc9c814ede0ac00ec8d58a04011315e0a5c4067f
Alicia <Alis@perlas-mbp.attlocal.net>
2017-01-22 20:52:39 -0600

    liri-bot1.0

---
+++ keys.js
@@ -0,0 +1,15 @@
+console.log('this is loaded');
+
+exports.prueba = {
+	prueba_uno:'<se imprime>',
+};
+
+
+
+exports.twitterKeys = {
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '-uBYWb8A1XeoLyZYDjBsvV36XELcilCcFE2IvfzWD',
+  access_token_secret: '',
+};
+
